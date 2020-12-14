1b1e1cd6bcc173161f0a9024c6486d14e1f620f5
ahmedhabeeb73 <alnaib888@gmail.com>
2020-12-14 14:39:55 +0100

    setup

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+
+
+let mainWindow;
+
+function createMainWindow()
+{
+  mainWindow=new BrowserWindow({
+    title:"ImageShrink",
+    width:500,
+    height:600,
+    icon:'./assets/Icon_256x256.png'
+  })
+
+  mainWindow.loadFile('./app/index.html');
+}
+
+app.on('ready',createMainWindow)
